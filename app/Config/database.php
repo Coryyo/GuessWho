46e429a8ef69e607d9eada16dc033d960f1505f4
Yoann Correia <yoanncorreia@gmail.com>
2015-04-01 13:44:17 +0200

    Ajout de la base de donnée.

---
+++ app/Config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+class DATABASE_CONFIG {
+
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'yoanncorreia.fr',
+		'login' => 'guesswho',
+		'password' => 'guesswho',
+		'database' => 'guesswho',
+		'prefix' => '',
+		//'encoding' => 'utf8',
+	);
+
+}
